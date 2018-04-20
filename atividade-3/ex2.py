39032b2c2aa8fe2fa6fb6a99a270080b5d4b1b57
Micael Pimentel <micael.pimentel1@gmail.com>
2018-04-19 23:53:24 -0400

    solucao exercicio 2

---
+++ atividade-3/ex2.py
@@ -0,0 +1,9 @@
+# Universidade Federal do Amazonas
+# Instituto de Computação
+# Introducação a Computação
+# Prof Alberto Castro
+# Atividade 3 - Exercício 2
+# Aluno: Micael S Pimentel
+# Data: 20/04/2018
+
+from math import *
