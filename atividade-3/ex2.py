641bfd2de7d488152b3f09d7364dcf97c563f077
Micael Pimentel <micael.pimentel1@gmail.com>
2018-04-20 11:34:40 -0400

    solucao exercicio 2

--- atividade-3/ex2.py
+++ atividade-3/ex2.py
@@ -8,13 +8,15 @@
 
 from math import *
 
-# dados nota final e frequencia, verifica se aprovado
+# verifica se aprovado
 def aprovado(nf,freq):
 	return (nf >= 5) and (freq >= 75)
 
+# reprovado por nota
 def reprovadoNota(nf):
 	return nf < 5
 
+# reprovado por falta
 def reprovadoFalta(freq):
 	return freq < 75
 
@@ -30,7 +32,10 @@ def reprovadosNota(nfs):
 def reprovadosFalta(nfs):
 	return [(n,f,s) for (n,f,s) in nfs if reprovadoFalta(s)]
 
+# retorna lista de tuplas com alunos reprovados por nota e falta
 def reprovadosNotaFalta(nfs):
 	return [(n,f,s) for (n,f,s) in nfs if not aprovado(f,s)]
 
-#def rfinal(nfs):
+# solucao final
+def rfinal(nfs):
+	return [aprovados(nfs),reprovadosNota(nfs),reprovadosFalta(nfs),reprovadosNotaFalta(nfs)]
