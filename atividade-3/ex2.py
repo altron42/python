f69282275073ab51814a354222a793f23de753b5
Micael Pimentel <micael.pimentel1@gmail.com>
2018-04-20 10:36:51 -0400

    lista com alunos aprovados

--- atividade-3/ex2.py
+++ atividade-3/ex2.py
@@ -7,3 +7,19 @@
 # Data: 20/04/2018
 
 from math import *
+
+# dados nota final e frequencia, verifica se aprovado
+def aprovado(nf,freq):
+	if (nf >= 5) and (freq >= 75):
+		return True
+	else:
+		return False
+
+# retorna lista de tuplas com alunos aprovados
+def aprovados(nfs):
+	return [(n,f,s) for (n,f,s) in nfs if aprovado(f,s)]
+
+
+
+def rfinal(nfs):
+	
