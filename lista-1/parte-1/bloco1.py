46b5c80187977331fa4f11229a4385d383cfb43e
Micael Pimentel <micael.pimentel1@gmail.com>
2018-05-04 22:37:40 -0400

    comeca bloco 1

---
+++ lista-1/parte-1/bloco1.py
@@ -0,0 +1,11 @@
+# Universidade Federal do Amazonas
+# Instituto de Computação
+# Introducação a Computação
+# Prof Alberto Castro
+# Lista 1 - Parte 1 - Bloco 1
+# Aluno: Micael S Pimentel
+# Data: 05/05/2018
+
+from math import *
+
+
