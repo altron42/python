90a48136a0e43497178c9c1714b7a8526fb4e7d1
Micael Pimentel <micael.pimentel1@gmail.com>
2018-05-05 07:21:46 -0400

    feito P01

--- lista-1/parte-1/bloco1.py
+++ lista-1/parte-1/bloco1.py
@@ -8,4 +8,13 @@
 
 from math import *
 
+# P01
+def pedrap(a,b):
+	if inteirop(a) and inteirop(b) and valorp(a) and valorp(b): return True
+	else: return False
 
+def inteirop(a):
+	return isinstance(a,int)
+
+def valorp(a):
+	return not ((a < 0) or (a >6))
