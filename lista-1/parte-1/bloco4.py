522865c5576d898c298ffc4fec662f03e70a6d28
Micael Pimentel <micael.pimentel1@gmail.com>
2018-05-27 21:27:57 -0400

    criado arquivo bloco4

---
+++ lista-1/parte-1/bloco4.py
@@ -0,0 +1,7 @@
+# Universidade Federal do Amazonas
+# Instituto de Computação
+# Introducação a Computação
+# Prof Alberto Castro
+# Lista 1 - Parte 1 - Bloco 4
+# Aluno: Micael S Pimentel
+# Data: 25/05/2018
